95750e19c5194e8e82d7470d5e876269817c00e0
cameron-chromiak <cam.chromiak@hotmail.com>
2019-04-16 11:11:16 -0400

    changed nav

--- app.js
+++ app.js
@@ -23,5 +23,5 @@ app.get('/', (req, res) =>{
   res.render('index')
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`App runnign on ${PORT} `))
